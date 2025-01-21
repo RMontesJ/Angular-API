a291841d41fbfe235e961ebf4bd26b29309e66b1
RMontesJ <148756305+RMontesJ@users.noreply.github.com>
2025-01-21 10:01:05 +0100

    Modules quitado

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  posts: any[] = [];
+  constructor(private servicio:DataService){ // inyectar el servicio - inyeccion de dependencias
+   
+  }
+  ngOnInit(){
+    this.servicio.getPosts().subscribe((data:any[]) =>{
+      this.posts = data;
+      console.log(this.posts);
+    })
+  }
+}
